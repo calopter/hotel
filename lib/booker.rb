ef1193c5634e842bdbc9d0aacf1c3e24a562aaa0
Mira <calopter@gmail.com>
2019-09-03 13:13:21 -0700

    initializes rooms

--- lib/booker.rb
+++ lib/booker.rb
@@ -1,5 +1,9 @@
 module Hotel
   class Booker
-    
+    attr_reader :rooms
+
+    def initialize
+      @rooms = (1..20).to_a
+    end
   end
 end
--- test/booker_test.rb
+++ test/booker_test.rb
@@ -1,7 +1,16 @@
 require_relative 'test_helper.rb'
 
 describe 'Booker' do
+  before do
+    @booker = Hotel::Booker.new
+  end
+  
   it 'is a Booker' do
-    expect(Hotel::Booker.new).must_be_instance_of Hotel::Booker
+    expect(@booker).must_be_instance_of Hotel::Booker
+  end
+
+  it 'can return all its rooms' do
+    expect(@booker.rooms).must_be_instance_of Array
+    expect(@booker.rooms.count).must_equal 20
   end
 end
