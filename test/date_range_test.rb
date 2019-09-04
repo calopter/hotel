5e2bc30d74ca9ec173367aad3a7fd7db1d1e7aa3
Mira <calopter@gmail.com>
2019-09-04 15:01:21 -0700

    adds tests to #overlaps? and @includes?

--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -2,20 +2,22 @@ require_relative 'test_helper'
 
 describe 'DateRange' do
   before do
-    @start = Date.jd 2458730
-    @end = Date.jd 2458731
+    @base_date = Date.jd 2458730 
+    @start = @base_date
+    @end = @base_date.next
     
-    @date_range = Hotel::DateRange.new @start, @end
+    @one_night = Hotel::DateRange.new @start, @end
+    @two_nights = Hotel::DateRange.new @start, @end.next
   end
 
   describe '#initialize' do
     it 'is a DateRange' do
-      expect(@date_range).must_be_instance_of Hotel::DateRange
+      expect(@one_night).must_be_instance_of Hotel::DateRange
     end
 
     it 'accepts and returns a start and an end' do
-      expect(@date_range.start).must_be_instance_of Date    
-      expect(@date_range.end).must_be_instance_of Date    
+      expect(@one_night.start).must_be_instance_of Date    
+      expect(@one_night.end).must_be_instance_of Date    
     end
 
     it 'raises ArgumentError if start is after end' do
@@ -25,33 +27,35 @@ describe 'DateRange' do
 
   describe '#includes?' do
     it 'returns true if date is in the date range' do
-      day = Date.jd 2458730
-      expect(@date_range.includes? day).must_equal true
+      expect(@one_night.includes? @base_date).must_equal true
+      expect(@one_night.includes? @end).must_equal true
     end
 
     it 'returns false if date is not in the date range' do
       day = Date.jd 3
-      expect(@date_range.includes? day).must_equal false
+      expect(@one_night.includes? day).must_equal false
+      expect(@one_night.includes? @end.next).must_equal false
     end
   end
 
   describe '#nights' do
     it 'returns a set of Date instances' do
-      expect(@date_range.nights).must_be_instance_of Set
-      expect(@date_range.nights.count).must_equal 1
-      expect(@date_range.nights.first).must_be_instance_of Date
+      expect(@one_night.nights).must_be_instance_of Set
+      expect(@one_night.nights.count).must_equal 1
+      expect(@one_night.nights.first).must_be_instance_of Date
     end
   end
 
   describe '#overlaps?' do
     it 'returns true if the ranges overlap' do
       other_range = Hotel::DateRange.new(@start, @end.succ)
-      expect(@date_range.overlaps? other_range).must_equal true 
+      expect(@one_night.overlaps? other_range).must_equal true 
     end
 
     it 'returns false if the ranges do not overlap' do
-      other_range = Hotel::DateRange.new(@end, @end.succ)
-      expect(@date_range.overlaps? other_range).must_equal false
+      after = Hotel::DateRange.new(@end, @end.next)
+      before = Hotel::DateRange.new(@start.prev_day, @start)
+      expect(@one_night.overlaps? after).must_equal false
     end
   end
 end
