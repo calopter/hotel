e36ed4479e6986352762e9525df27c5547fbab17
Mira <calopter@gmail.com>
2019-09-05 16:14:12 -0700

    adds tests for reserving within a block

--- lib/block.rb
+++ lib/block.rb
@@ -1,7 +1,13 @@
 module Hotel
   class Block < Booker
-    def initialize date_range, rooms, rate
-      
+    def initialize date_range, rooms, rate, revervations=[]
+      super rooms, rate
+      @date_range = date_range
+      @reservations = revervations
+    end
+
+    def reserve
+      super @date_range
     end
   end
 end
--- test/block_test.rb
+++ test/block_test.rb
@@ -2,13 +2,49 @@ require_relative 'test_helper'
 
 describe 'Block' do
   before do
-    date_range = Hotel::DateRange.new Date.today, Date.today.next
-    rooms = (1..5).to_a
+    @start = Date.today
+    @end = Date.today.next
+    @short_stay = Hotel::DateRange.new @start, @end
+    @rooms = (1..5).to_a
     rate = 100
-    @block = Hotel::Block.new date_range, rooms, rate
+    @block = Hotel::Block.new @short_stay, @rooms, rate
   end
   
-  it 'is an instance of Block' do
-    expect(@block).must_be_instance_of Hotel::Block
+  describe '#initialize' do
+    it 'is an instance of Block' do
+      expect(@block).must_be_instance_of Hotel::Block
+    end
+
+    it 'has superclass methods' do
+      [:reserve, :add_reservation, :reservations_on, :available?, :availabilities, :reservations].each do |method|
+        expect(@block).must_respond_to method
+      end
+    end
+  end
+
+  describe '#reserve' do
+    it 'stores and returns a reservation' do
+      new_reservation = @block.reserve
+      that_reservation = @block.reservations.first
+      
+      expect(that_reservation).must_be_instance_of Hotel::Reservation
+      expect(that_reservation).must_equal new_reservation
+      expect(that_reservation.date_range).must_equal @short_stay
+    end
+
+    it 'picks a room in the block' do
+      5.times { expect(@rooms).must_include @block.reserve.room }
+    end
+
+    it 'picks an available room' do
+      4.times { @block.reserve }
+      reservation = @block.reserve 
+
+      expect(reservation.room).must_equal @block.rooms.last    end
+
+    it 'raises RuntimeError if no availabilities in block' do
+      5.times { @block.reserve }
+      expect{ @block.reserve }.must_raise RuntimeError
+    end
   end
 end
