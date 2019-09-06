f3edf249d46a6c96dba97f6012a90d301a5bcfa6
Mira <calopter@gmail.com>
2019-09-05 19:47:41 -0700

    implements #reserve

--- lib/block.rb
+++ lib/block.rb
@@ -1,7 +1,15 @@
 module Hotel
   class Block < Booker
-    def initialize id:, date_range:, rooms:, rate:, reservations:
-      
+    attr_reader :date_range
+    
+    def initialize id:, date_range:, rooms:, rate: 100, reservations: 
+      super(rooms: rooms, rate: rate)
+      @date_range = date_range
+      @reservations = reservations
+    end
+
+    def reserve
+      super @date_range
     end
   end
 end
--- test/block_test.rb
+++ test/block_test.rb
@@ -4,12 +4,54 @@ describe 'Block' do
   before do
     date_range = Hotel::DateRange.new Date.today, Date.today.next
     rooms = (1..5).to_a
-    rate = 100
     @booker = Hotel::Booker.new(rooms: (1..20).to_a)
-    @block = Hotel::Block.new(id: 0, date_range: date_range, rooms: rooms, rate: rate, reservations: @booker.reservations)
+    @block = Hotel::Block.new(id: 0, date_range: date_range, rooms: rooms, reservations: @booker.reservations)
   end
-  
-  it 'is an instance of Block' do
-    expect(@block).must_be_instance_of Hotel::Block
+
+  describe 'initialize' do
+    it 'is an instance of Block' do
+      expect(@block).must_be_instance_of Hotel::Block
+    end
+
+    it 'has superclass methods' do
+      [:reserve, :add_reservation, :reservations_on,
+      :available?, :availabilities, :reservations].each do |method|
+        expect(@block).must_respond_to method
+      end
+    end
+  end
+
+  describe '#reserve' do
+    it 'stores and returns a reservation' do
+      new_reservation = @block.reserve
+      that_reservation = @block.reservations.first
+
+      expect(that_reservation).must_be_instance_of Hotel::Reservation
+      expect(that_reservation).must_equal new_reservation
+      expect(that_reservation.date_range).must_equal @block.date_range
+    end
+
+    it 'stores that reservation such that Booker can see it' do
+      reservation = @block.reserve
+      expect(@booker.reservations.count).must_equal 1
+      expect(@booker.reservations).must_include reservation
+    end
+
+    it 'picks a room in the block' do
+      @block.rooms.count.times do
+        expect(@block.rooms).must_include @block.reserve.room
+      end
+    end
+
+    it 'picks an available room' do
+      4.times { @block.reserve }
+      reservation = @block.reserve
+      expect(reservation.room).must_equal @block.rooms.last
+    end
+
+    it 'raises RuntimeError if no availabilities in block' do
+      5.times { @block.reserve }
+      expect{ @block.reserve }.must_raise RuntimeError
+    end
   end
 end
